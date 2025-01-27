785b3d267c52a7dbd175869900415fa0fa493cde
wallan_munhoz <wallandeividy269@gmail.com>
2025-01-27 16:23:31 -0300

    first commit

---
+++ caixa_teste/fubction/function.js
@@ -0,0 +1,19 @@
+
+function appendValue(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Erro';
+    }
+}
